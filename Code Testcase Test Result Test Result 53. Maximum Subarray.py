7433899613620dfc41161302f5bd4cc6b80f2c35
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-26 19:06:48 +0530

    Create  Code Testcase Test Result Test Result 53. Maximum Subarray.py

---
+++ Code Testcase Test Result Test Result 53. Maximum Subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_so_far = nums[0]
+        max_ending_here = nums[0]
+        
+        for num in nums[1:]:
+            max_ending_here = max(num, max_ending_here + num)
+            max_so_far = max(max_so_far, max_ending_here)
+        
+        return max_so_far
