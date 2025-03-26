5dc5d78d6d25c77565fdd25deb05a39ae8b0b01e
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-26 19:10:06 +0530

    Create 60. Permutation Sequence.py

---
+++ 60. Permutation Sequence.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        from math import factorial
+
+        # Create a list of numbers to get indices
+        numbers = list(range(1, n + 1))
+        # Adjust k to be zero-indexed
+        k -= 1
+        # Initialize the result
+        result = []
+
+        # Generate the permutation
+        for i in range(n, 0, -1):
+            # Determine the index of the current digit
+            index, k = divmod(k, factorial(i - 1))
+            result.append(str(numbers[index]))
+            # Remove used number
+            numbers.pop(index)
+
+        return ''.join(result)
