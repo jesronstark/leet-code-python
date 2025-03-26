bf513e11cb421aa4155d6ec81abd057485f3ae26
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-26 19:13:42 +0530

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n, memo={}):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n in memo:
+            return memo[n]
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            memo[n] = self.climbStairs(n - 1, memo) + self.climbStairs(n - 2, memo)
+            return memo[n]
