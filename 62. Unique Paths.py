22704ed7cdaf6345a61ba71872039544faba508e
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-26 19:10:50 +0530

    Create 62. Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # Initialize a 2D DP table with 1s for the first row and first column
+        dp = [[1] * n for _ in range(m)]
+        
+        # Fill the DP table
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
