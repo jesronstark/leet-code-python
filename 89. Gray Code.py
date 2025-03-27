3bc9db190ef45c428e25d8995ddb191fbafb1796
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-27 18:55:28 +0530

    Create 89. Gray Code.py

---
+++ 89. Gray Code.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = [0]
+        for i in range(n):
+            result += [x + (1 << i) for x in reversed(result)]
+        return result
